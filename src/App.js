5d2124282c39ef27eab3cbedbaae83599de72b2d
noufanelachola <noufansadiqelachola@gmail.com>
2023-07-15 18:06:49 +0530

    initially completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React ,{Component} from 'react';
+import tachyons from 'tachyons';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      count : 0
+    }
+  }
+
+  increase = () => {
+    this.setState({count: this.state.count + 1})
+  }
+  decrease = () => {
+    this.setState({count: this.state.count - 1})
+  }
+
+  render(){
+    return (
+      <div className="parent" >
+        <h1>{this.state.count}</h1>
+        <div className="btn">
+          <button onClick={this.increase} >Increase</button>
+          <button onClick={this.decrease}>Decrease</button>
+        </div>
+      </div>
+      );
+  }
 }
 
 export default App;
